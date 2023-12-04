f62451f790d6c3ddd02a018001e498f26fdca599
Raymond-Fries <112507745+Raymond-Fries@users.noreply.github.com>
2023-12-04 14:27:36 -0500

    Update upcoming_stock_markov.py

--- upcoming_stock_markov.py
+++ upcoming_stock_markov.py
@@ -1,18 +1,12 @@
 import numpy as np
 import pandas as pd
 from random import seed
-import psycopg2
 import os
 import matplotlib.pyplot as plt
 from datetime import datetime,timedelta
 from sklearn.preprocessing import PowerTransformer
 from data_generation import get_all_holidays_csv,get_all_fed_dates_csv,get_all_er_dates_csv
 
-
-def get_historical_df(symbol,dayofweek):
-    df = pd.read_csv('Data/Daily_Intraday/Daily_Means/'+symbol.upper()+'_'+str(dayofweek)+'_daily_minute_means.csv',header=0)
-    df = df.drop(['Unnamed: 0'],axis=1)
-    return df
 
 def get_close_over_close_boundaries(symbol,dayofweek,stds):
     df = pd.read_csv('Data/Garch_Results/'+symbol+'_'+str(dayofweek)+'_'+str(stds)+'stds_Close_over_Close_Garch_Results.csv')
@@ -46,24 +40,21 @@ def get_previous_close(symbol,last_date):
     temp = df[(df['symbol'] == symbol) & (df['dates'] == dates[last_close_index-1])].reset_index()
     return temp['close'].iloc[0]
 
+## sets decimal places
+## removes all other decimal values after
+## the desired place value, in this case
+## 2 decimal places
 def truncate(df, decimals=0):
     multiplier = 10 ** decimals
     df = (df * multiplier) / multiplier
     return df
 
-def truncate_to_even_cents(df,previous_close):
-    df = df.cumsum(axis=0)
-    df = previous_close + (df * previous_close)
-    df = df.round(2)
-    new_row = pd.Series([previous_close] * len(df.columns))
-    temp = new_row.to_frame().T
-    df = truncate(df,2)
-    df2 = pd.concat([temp,df],axis=0).reset_index(drop=True)
-    df = pd.DataFrame()
-    temp= pd.DataFrame()
-    df2 = df2.pct_change()
-    df2 = df2.dropna(axis=0)
-    return df2
+## Stocks are traded in .01 cent intervals
+## this function takes the simulated changes
+## sums them, then multiplies by stocks previous close
+## to generate values in .01 cent intervals, then changes
+## back into percentage changes to mimic real market possibilities
+## similar to a markov with a beginning state of the previous close price
 def get_data(symbol,dayofweek,time):
     df = pd.read_csv('Data/Daily_Intraday/'+symbol.upper()+'_intraday.csv',header=0)
     df = df.drop(['Unnamed: 0'],axis=1)
@@ -95,7 +86,13 @@ def get_general_data(symbol,dayofweek,time):
     temp = temp.drop('date',axis=1)
     df = pd.DataFrame()
     return temp
-
+    
+## create simulation using day of week probabilities
+## with no phenomena
+## set at 850,000 random normal values(columns)
+## for 390 minutes. 390 rows by 850000 column simulation
+## simulation gets slimmed down by constraints
+## to mimic days possible paths within predicted characteristics
 def create_random_probs(symbol,dayofweek,stds):
     
     ccb = get_close_over_close_boundaries(symbol,dayofweek,stds)
@@ -140,14 +137,13 @@ def create_random_probs(symbol,dayofweek,stds):
         df = pd.DataFrame()
         
         most_likely = cents.loc[:,(cents.std() <= std_upper) & (cents.std() >= std_lower) & (cents.mean() <= mean_upper) & (cents.mean() >= mean_lower) & (cents.iloc[0] <= open_upper) & (cents.iloc[0] >= open_lower) & (cents.cumsum().iloc[389] <= eod_upper) & (cents.cumsum().iloc[389] >= eod_lower)]
-        #most_likely = most_likely.drop('index',axis=1)
-        
+
         cents = pd.DataFrame()
         print(symbol,' count1 ',count1,' ',dayofweek,' ',str(stds),' ',len(most_likely.columns),' ',most_likely.mean().min(),' ',most_likely.mean().max(),' ',most_likely.std().min(),' ',most_likely.std().max())
         if len(most_likely.columns) > 0:
 
             for i in most_likely.columns:
-                #requirements_df[i].mean() <= mean_upper and requirements_df[i].mean() >= mean_lower and
+               
                 name = str(i)
                 plt.plot(most_likely[i].cumsum(),alpha=.35,label=name+'_Simulation')
                 count += 1
@@ -156,7 +152,7 @@ def create_random_probs(symbol,dayofweek,stds):
         print(datetime.now()-start)
     plt.title(symbol+' '+str(dayofweek)+' Simulation with '+str(stds)+' standard deviation for all constraints')
     #plt.savefig('Data/Simulations_Images/'+symbol+'_'+str(dayofweek)+'_'+str(stds)+'_No_Phenomena_Constraints_SVR-GARCH.png')
-    plt.show()
+    #plt.show()
     #plt.close()
     
 def general_create_random_probs(symbol,dayofweek,stds):
@@ -198,13 +194,13 @@ def general_create_random_probs(symbol,dayofweek,stds):
                 count += 1
             print('count: ',count)
         cents = pd.DataFrame()
-    plt.title(symbol+' '+str(dayofweek)+' Simulation with No Constraints')
-    plt.savefig('Data/Simulations_Images/'+symbol+'_'+str(dayofweek)+'_General_Only_Day_of_Week_Subset_No_Constraints_SVR-GARCH.png')
-    plt.close()
+##    plt.title(symbol+' '+str(dayofweek)+' Simulation with No Constraints')
+##    plt.savefig('Data/Simulations_Images/'+symbol+'_'+str(dayofweek)+'_General_Only_Day_of_Week_Subset_No_Constraints_SVR-GARCH.png')
+##    plt.close()
 
     #return requirements_df 'AMD',
-for symbol in ['GOOG']:
-    for i in range(0,1,1):
-        for j in range(2,3,1):
-            create_random_probs(symbol,i,j)
-            #general_create_random_probs(symbol,i,j)
+##for symbol in ['GOOG']:
+##    for i in range(0,1,1):
+##        for j in range(2,3,1):
+##            create_random_probs(symbol,i,j)
+##            general_create_random_probs(symbol,i,j)
